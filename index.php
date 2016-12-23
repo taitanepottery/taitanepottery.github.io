8e30a5b8e86df638e576b4a833afaa01f0966811
millen1m <maxim.millen@gmail.com>
2016-12-23 17:15:32 +1300

    added bottstrap theme

---
+++ index.php
@@ -0,0 +1,368 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8" />
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+<title>Tucon bicycle</title>
+
+<!-- Google fonts -->
+<link href='http://fonts.googleapis.com/css?family=Raleway:400,800' rel='stylesheet' type='text/css'>
+
+<!-- font awesome -->
+<link href="//maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
+
+<!-- bootstrap -->
+<link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css" />
+
+<!-- animate.css -->
+<link rel="stylesheet" href="assets/animate/animate.css" />
+
+<!-- gallery -->
+<link rel="stylesheet" href="assets/gallery/blueimp-gallery.min.css">
+
+<!-- favicon -->
+<link rel="shortcut icon" href="images/favicon.png" type="image/x-icon">
+<link rel="icon" href="images/favicon.png" type="image/x-icon">
+
+
+<link rel="stylesheet" href="assets/style.css">
+
+</head>
+
+<body id="home">
+<!-- header -->
+      
+       
+
+      <h1 class="logo"><a href="#home" class="scroll">Tucon bicycle</a></h1>
+      <div class="menu">
+      <a href="#about" class="scroll">About</a>
+      <a href="#products" class="scroll">Products</a>      
+      <a href="#contact" class="scroll">Contact</a>      
+      </div>
+      <!-- slider -->
+      <div id="carousel-banner" class="carousel slide" data-ride="carousel">
+      <div class="carousel-inner" role="listbox">
+        <div class="item active"><img src="images/banner1.jpg" class="img-responsive">
+               <!-- caption -->
+              <div class="caption">
+              <h2>Smooth Gears</h2>
+              <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+              </p>   
+              </div>
+              <!-- caption -->
+        </div>
+        <div class="item"><img src="images/banner2.jpg" class="img-responsive">
+               <!-- caption -->
+              <div class="caption">
+              <h2>Smooth Gears</h2>
+              <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+              </p>   
+              </div>
+              <!-- caption -->
+        </div>
+        <div class="item"><img src="images/banner3.jpg" class="img-responsive">
+              <!-- caption -->
+              <div class="caption">
+              <h2>Smooth Gears</h2>
+              <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+              </p>   
+              </div>
+              <!-- caption -->
+        </div>        
+      </div>         
+      <!-- Controls -->
+      <div class="next-prev">
+      <a class="left" href="#carousel-banner" role="button" data-slide="prev"><i class="fa fa-3x fa-angle-left"></i></a>
+      <a class="right" href="#carousel-banner" role="button" data-slide="next"><i class="fa fa-3x fa-angle-right"></i></a>
+      </div>
+      </div>
+      
+      <!-- slider -->
+
+
+
+<!-- about -->
+<div class="about" id="about">
+  <div class="container">
+    <h2 class="text-center">Know what makes us different</h2>
+    <div class="row">
+    <div class="col-md-6 col-xs-12 col-md-offset-3">
+
+        <div class="panel-group" id="accordion" role="tablist" aria-multiselectable="true">
+        <div class="panel panel-default">
+        <div class="panel-heading" role="tab">
+        <h4 class="panel-title">
+          <a data-toggle="collapse" data-parent="#accordion" href="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+           <i class="fa fa-bicycle"></i> What are the special features
+          </a>
+        </h4>
+        </div>
+        <div id="collapseOne" class="panel-collapse collapse" role="tabpanel">
+        <div class="panel-body">
+         Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.              
+        </div>
+        </div>
+        </div>
+        <div class="panel panel-default">
+        <div class="panel-heading" role="tab">
+        <h4 class="panel-title">
+          <a class="collapsed" data-toggle="collapse" data-parent="#accordion" href="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+           <i class="fa fa-bicycle"></i> The record that we have set so far on sales
+          </a>
+        </h4>
+        </div>
+        <div id="collapseTwo" class="panel-collapse collapse" role="tabpanel">
+        <div class="panel-body">
+         Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.              
+        </div>
+        </div>
+        </div>
+        <div class="panel panel-default">
+        <div class="panel-heading" role="tab">
+        <h4 class="panel-title">
+          <a class="collapsed" data-toggle="collapse" data-parent="#accordion" href="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+           <i class="fa fa-bicycle"></i> Who uses our products most
+          </a>
+        </h4>
+        </div>
+        <div id="collapseThree" class="panel-collapse collapse" role="tabpanel">
+        <div class="panel-body">
+         Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.              
+        </div>
+        </div>
+        </div>
+
+        <div class="panel panel-default">
+        <div class="panel-heading" role="tab">
+        <h4 class="panel-title">
+          <a class="collapsed" data-toggle="collapse" data-parent="#accordion" href="#collapseFour" aria-expanded="false" aria-controls="collapseFour">
+           <i class="fa fa-bicycle"></i> Price Ranges
+          </a>
+        </h4>
+        </div>
+        <div id="collapseFour" class="panel-collapse collapse" role="tabpanel">
+        <div class="panel-body">
+           <div class="clearfix food-list"><div class="pull-left">Bicycle</div><span class="pull-right">$ 10.00</span></div>
+          <div class="clearfix food-list"><div class="pull-left">Mountains </div><span class="pull-right">$ 10.00</span></div>
+          <div class="clearfix food-list"><div class="pull-left">Old Cycle</div><span class="pull-right">$ 10.00</span></div>
+          <div class="clearfix food-list"><div class="pull-left">New Brands</div><span class="pull-right">$ 10.00</span></div>
+        </div>
+        </div>
+        </div>
+
+      
+
+
+        </div>
+        </div>     
+    </div>
+  </div>
+</div>
+<!-- about -->
+      
+
+
+       <!-- product list -->
+       <div id="products">
+       <h2 class="text-center">Our Products</h2>
+        <div class="row-fluid gallery clearfix" id="portfolio">
+              <!-- product -->
+              <div class="col-sm-3 product wowload fadeInUp">
+                <img src="images/photos/1.jpg" class="img-responsive">
+                <div class="overlay">
+                  <div class="ab-wrap"><div class="ab-center text-center">
+                    <h3>Casual Converse</h3>
+                    <p>$ 20.00</p>
+                    <a href="images/photos/1.jpg" title="Shoes" class="btn btn-default gallery-image" data-gallery="">View Preview</a>
+                  </div></div>
+                </div>
+              </div>
+              <!-- product -->
+
+              <!-- product -->
+              <div class="col-sm-3 product wowload fadeInUp">
+                <img src="images/photos/2.jpg" class="img-responsive">
+                <div class="overlay">
+                  <div class="ab-wrap"><div class="ab-center text-center">
+                    <h3>Casual Converse</h3>
+                    <p>$ 20.00</p>
+                    <a href="images/photos/2.jpg" title="Formal" class="btn btn-default gallery-image" data-gallery="">View Preview</a>
+                  </div></div>
+                </div>
+              </div>
+              <!-- product -->
+
+              <!-- product -->
+              <div class="col-sm-3 product wowload fadeInUp">
+                <img src="images/photos/3.jpg" class="img-responsive">
+                <div class="overlay">
+                  <div class="ab-wrap"><div class="ab-center text-center">
+                    <h3>Casual Converse</h3>
+                    <p>$ 20.00</p>
+                    <a href="images/photos/3.jpg" title="Red" class="btn btn-default gallery-image" data-gallery="">View Preview</a>
+                  </div></div>
+                </div>
+              </div>
+              <!-- product -->
+
+              <!-- product -->
+              <div class="col-sm-3 product wowload fadeInUp ">
+                <img src="images/photos/4.jpg" class="img-responsive">
+                <div class="overlay">
+                  <div class="ab-wrap"><div class="ab-center text-center">
+                    <h3>Casual Converse</h3>
+                    <p>$ 20.00</p>
+                    <a href="images/photos/4.jpg" title="Brown" class="btn btn-default gallery-image" data-gallery="">View Preview</a>
+                  </div></div>
+                </div>
+              </div>
+              <!-- product -->
+
+
+
+              <!-- product -->
+              <div class="col-sm-3 product wowload fadeInUp ">
+                <img src="images/photos/4.jpg" class="img-responsive">
+                <div class="overlay">
+                  <div class="ab-wrap"><div class="ab-center text-center">
+                    <h3>Casual Converse</h3>
+                    <p>$ 20.00</p>
+                    <a href="images/photos/4.jpg" title="Brown" class="btn btn-default gallery-image" data-gallery="">View Preview</a>
+                  </div></div>
+                </div>
+              </div>
+              <!-- product -->
+
+
+              <!-- product -->
+              <div class="col-sm-3 product wowload fadeInUp">
+                <img src="images/photos/3.jpg" class="img-responsive">
+                <div class="overlay">
+                  <div class="ab-wrap"><div class="ab-center text-center">
+                    <h3>Casual Converse</h3>
+                    <p>$ 20.00</p>
+                    <a href="images/photos/3.jpg" title="Red" class="btn btn-default gallery-image" data-gallery="">View Preview</a>
+                  </div></div>
+                </div>
+              </div>
+              <!-- product -->
+
+
+
+              <!-- product -->
+              <div class="col-sm-3 product wowload fadeInUp">
+                <img src="images/photos/1.jpg" class="img-responsive">
+                <div class="overlay">
+                  <div class="ab-wrap"><div class="ab-center text-center">
+                    <h3>Casual Converse</h3>
+                    <p>$ 20.00</p>
+                    <a href="images/photos/1.jpg" title="Shoes" class="btn btn-default gallery-image" data-gallery="">View Preview</a>
+                  </div></div>
+                </div>
+              </div>
+              <!-- product -->
+
+              <!-- product -->
+              <div class="col-sm-3 product wowload fadeInUp">
+                <img src="images/photos/2.jpg" class="img-responsive">
+                <div class="overlay">
+                  <div class="ab-wrap"><div class="ab-center text-center">
+                    <h3>Casual Converse</h3>
+                    <p>$ 20.00</p>
+                    <a href="images/photos/2.jpg" title="Formal" class="btn btn-default gallery-image" data-gallery="">View Preview</a>
+                  </div></div>
+                </div>
+              </div>
+              <!-- product -->
+       </div>
+       </div>
+       <!-- product list -->
+
+
+       <!-- contact -->
+       <div class="contact" id="contact">
+
+       <div class="container">
+       <h2 class="text-center">If you like our products then contact us for details</h2>
+       <div class="row">
+        <div class="col-sm-6 col-sm-offset-3">
+          <form>
+                <div class="form-group">
+                    <input id="name" name="name" type="text" class="form-control" placeholder="Name"> 
+                </div>
+                <div class="form-group">
+                    <input id="email" name="email" type="email" class="form-control" placeholder="Email address">
+                </div>
+                <div class="form-group">
+                    <textarea id="message" name="message" class="form-control" placeholder="Your Message" rows="5"></textarea>
+                </div>                  
+                <button type="submit" class="btn btn-warning"><i class="fa fa-send-o"></i> Submit</button>
+            </form>
+         
+          </div>
+        </div>
+       </div>
+       </div>
+
+       
+       <div id="map"></div>
+       <!-- contact -->
+
+
+        <!-- footer -->
+        <div class="footer text-center">
+          <div class="social">
+          <a href="#"><i class="fa fa-facebook fa-2x"></i></a>
+          <a href="#"><i class="fa fa-instagram fa-2x"></i></a>
+          <a href="#"><i class="fa fa-twitter fa-2x"></i></a>
+          <a href="#"><i class="fa fa-pinterest fa-2x"></i></a>
+          </div>
+        Powered by: <a href="http://thebootstrapthemes.com">www.thebootstrapthemes.com</a>
+        </div>
+        <!-- footer -->
+
+
+
+
+
+
+      <!-- The Bootstrap Image Gallery lightbox, should be a child element of the document body -->
+      <div id="blueimp-gallery" class="blueimp-gallery blueimp-gallery-controls">
+          <!-- The container for the modal slides -->
+          <div class="slides"></div>
+          <!-- Controls for the borderless lightbox -->
+          <h3 class="title">Title</h3>
+          <a class="prev">‹</a>
+          <a class="next">›</a>
+          <a class="close">×</a>
+          <!-- The modal dialog, which will be used to wrap the lightbox content -->    
+      </div>
+
+<a href="#home" class="gototop scroll"><i class="fa fa-angle-up  fa-3x"></i></a>
+
+<!-- jquery -->
+<script src="assets/jquery.js"></script>
+
+<!-- wow script -->
+<script src="assets/wow/wow.min.js"></script>
+
+<!-- boostrap -->
+<script src="assets/bootstrap/js/bootstrap.js" type="text/javascript" ></script>
+
+<!-- jquery mobile -->
+<script src="assets/mobile/touchSwipe.min.js"></script>
+<script src="assets/respond/respond.js"></script>
+
+<!-- gallery -->
+<script src="assets/gallery/jquery.blueimp-gallery.min.js"></script>
+
+
+ <script src='https://maps.googleapis.com/maps/api/js?key=&sensor=false&extension=.js'></script> 
+ 
+<!-- custom script -->
+<script src="assets/script.js"></script>
+
+</body>
+</html>
