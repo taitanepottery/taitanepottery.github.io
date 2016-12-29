f433324366b88e21fe0306e43093bdb5daa84c51
millen1m <maxim.millen@gmail.com>
2016-12-29 17:50:36 +1300

    tidied up image containers for portfolio

--- js/general.js
+++ js/general.js
@@ -6,7 +6,7 @@ var imgs = ['http://lorempizza.com/380/240',
             'http://dummyimage.com/250/ffffff/000000',
             'http://lorempixel.com/g/400/200/',
             'images/workshop/cleaning-bowls.jpg'];
-var container = document.getElementById('imageContainer-DELETE');
+var container = document.getElementById('imageContainer');
 console.log(container);
 
 for (var i = 0, j = imgs.length; i < j; i++) {
