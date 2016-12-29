108edfb84e2e11aa39db9abcf7f83e1d66ed3c3d
millen1m <maxim.millen@gmail.com>
2016-12-29 19:40:41 +1300

    scaled images to set width.

--- js/general.js
+++ js/general.js
@@ -2,19 +2,33 @@
  * Created by maximmillen on 29/12/16.
  */
 
-var imgs = ['http://lorempizza.com/380/240',
-            'http://dummyimage.com/250/ffffff/000000',
-            'http://lorempixel.com/g/400/200/',
-            'images/workshop/cleaning-bowls.jpg'];
-var container = document.getElementById('imageContainer');
-console.log(container);
 
-for (var i = 0, j = imgs.length; i < j; i++) {
-  console.log("img");
+var container = document.getElementById('raku-container');
+
+for (var i = 0, j = 5; i < j; i++) {
   var diver = document.createElement('div');
+  var modalButton = document.createElement('a');
+  modalButton.setAttribute('href',"#raku-container");
+  modalButton.className = "portfolio-button";
   diver.className = "col portfolio-item";
     var img = document.createElement('img');
-    img.src = imgs[i]; // img[i] refers to the current URL.
-    diver.appendChild(img);
+    img.src = "images/portfolio/raku-" + String(i) + ".jpg"; // img[i] refers to the current URL.
+    diver.appendChild(modalButton);
+  modalButton.appendChild(img);
     container.appendChild(diver);
+}
+
+var wallArt = document.getElementById('wall-art-container');
+
+for (var i = 0, j = 4; i < j; i++) {
+  var div_w = document.createElement('div');
+  div_w.className = "col portfolio-item";
+  var a_w = document.createElement('a');
+  a_w.setAttribute('href',"#wall-art-container");
+  a_w.className = "portfolio-button";
+    img = document.createElement('img');
+    img.src = "images/portfolio/wall-art-" + String(i) + ".jpg"; // img[i] refers to the current URL.
+    div_w.appendChild(a_w);
+    a_w.appendChild(img);
+    wallArt.appendChild(div_w);
 }
