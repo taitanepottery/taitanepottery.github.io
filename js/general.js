e44732bbddf6b91fc181e851e9177b5a82746b0f
millen1m <maxim.millen@gmail.com>
2017-02-17 17:33:21 +1300

    Major update to portfolio images and removed workshop section, also changing padding to have better spacing.

--- js/general.js
+++ js/general.js
@@ -2,19 +2,8 @@
  * Created by maximmillen on 29/12/16.
  */
 
-
-var container = document.getElementById('raku-container');
 var div_w = null;
 
-for (var i = 1, j = 7; i < j; i++) {
-  div_w = document.createElement('div');
-
-  div_w.className = "col portfolio-item";
-    var img = document.createElement('img');
-    img.src = "images/portfolio/raku-" + String(i) + ".jpg"; // img[i] refers to the current URL.
-    div_w.appendChild(img);
-    container.appendChild(div_w);
-}
 
 var wallArt = document.getElementById('wall-art-container');
 
@@ -22,18 +11,67 @@ for (var i = 1, j = 7; i < j; i++) {
   div_w = document.createElement('div');
   div_w.className = "col portfolio-item";
     img = document.createElement('img');
-    img.src = "images/portfolio/wall-art-" + String(i) + ".jpg"; // img[i] refers to the current URL.
+    img.src = "images/portfolio/wall-art_work/wall-art-" + String(i) + ".jpg"; // img[i] refers to the current URL.
     div_w.appendChild(img);
     wallArt.appendChild(div_w);
 }
 
-var container = document.getElementById('garden-container');
+var container = document.getElementById('functional-container');
 
-for (var i = 1, j = 4; i < j; i++) {
+for (var i = 1, j = 7; i < j; i++) {
   div_w = document.createElement('div');
   div_w.className = "col portfolio-item";
     img = document.createElement('img');
-    img.src = "images/portfolio/garden-" + String(i) + ".jpg"; // img[i] refers to the current URL.
+    img.src = "images/portfolio/functional_work/functional-" + String(i) + ".jpg"; // img[i] refers to the current URL.
+  //img.src = "images/portfolio/garden-" + String(i) + ".jpg"; // img[i] refers to the current URL.
+    div_w.appendChild(img);
+    container.appendChild(div_w);
+}
+
+var container = document.getElementById('raku-container');
+
+for (var i = 1, j = 7; i < j; i++) {
+  div_w = document.createElement('div');
+
+  div_w.className = "col portfolio-item";
+    var img = document.createElement('img');
+    img.src = "images/portfolio/raku_work/raku-" + String(i) + ".jpg"; // img[i] refers to the current URL.
+    div_w.appendChild(img);
+    container.appendChild(div_w);
+}
+
+
+var container = document.getElementById('garden-container');
+
+for (var i = 1, j = 7; i < j; i++) {
+  div_w = document.createElement('div');
+  div_w.className = "col portfolio-item";
+    img = document.createElement('img');
+    img.src = "images/portfolio/garden_work/garden-" + String(i) + ".jpg"; // img[i] refers to the current URL.
+    div_w.appendChild(img);
+    container.appendChild(div_w);
+}
+
+var container = document.getElementById('tall-container');
+
+for (var i = 1, j = 7; i < j; i++) {
+  div_w = document.createElement('div');
+  div_w.className = "col portfolio-item";
+    img = document.createElement('img');
+    img.src = "images/portfolio/tall_work/tall-" + String(i) + ".jpg"; // img[i] refers to the current URL.
+  //img.src = "images/portfolio/garden-" + String(i) + ".jpg"; // img[i] refers to the current URL.
+    div_w.appendChild(img);
+    container.appendChild(div_w);
+}
+
+var container = document.getElementById('colab-container');
+
+for (var i = 1, j = 7; i < j; i++) {
+  div_w = document.createElement('div');
+  div_w.className = "col portfolio-item";
+    img = document.createElement('img');
+    img.src = "images/portfolio/colab_work/colab-" + String(i) + ".jpg"; // img[i] refers to the current URL.
+  //img.src = "images/portfolio/garden-" + String(i) + ".jpg"; // img[i] refers to the current URL.
     div_w.appendChild(img);
     container.appendChild(div_w);
 }
